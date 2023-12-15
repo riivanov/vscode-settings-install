b5f1ac965f67484d214babd0d145092703a2324a
Radoslav Ivanov <riivanov63@gmail.com>
2023-12-15 23:14:56 +0200

    add list of extensions

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# If PS: Invoke-WebRequest $myDownloadUrl -OutFile c:\file.ext 
 
 # Detect OS type
 # https://www.twitch.tv/ewo_on_sight 
@@ -8,11 +7,14 @@ UNAME=$(uname)
 if [ "$UNAME" == "Linux" ] ; then
   VSCODEDIR="$HOME/.config/Code\ -\ OSS/User";
 elif [ "$UNAME" == "Darwin" ] ; then
-  #echo "Darwin";
   exit 2; #not supported
 elif [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]] ; then
   VSCODEDIR="$APPDATA/Code/User";
 fi
 
+# Download settings.json
 curl -s "https://raw.githubusercontent.com/riivanov/vscode-settings-install/main/settings.json" -o $VSCODEDIR/settings.json
+# Download keybindings.json
 curl -s "https://raw.githubusercontent.com/riivanov/vscode-settings-install/main/keybindings.json" -o $VSCODEDIR/keybindings.json
+
+
